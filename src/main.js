3693142098e29654ff001f4584751fd55ad73e24
Aaron Vitarana <aaron.vitarana.ext@bestsecret.com>
2024-05-03 13:53:53 +0200

    first commit

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ new App({
 	target: document.body,
 	props: {
 		// What's your name?
-		name: 'James Butler',
+		name: 'Aaron Vitarana',
 		// In the following fiels you can either give a single string, 
 		// or an array of bullet points
 		
@@ -13,16 +13,15 @@ new App({
 			'Ehm, what?'
 		],
 		// Which CI/CD tools do you use in your project?
-		tools: 'Jenkins, maybe?',
+		tools: 'Jenkins, Gitlab CI/CD',
 		// What do you want to learn in this workshop? 
 		expectations: [
-			'what you are talking about'
+			'Deeper understanding about CI/CD'
 		],
 		// What do you like to do when you're not coding?
 		hobbies: [
-			'play chess', 
-			'do skydiving', 
-			'do my taxes'
+			'football',
+			'bouldering'
 		]
 	}
 });
